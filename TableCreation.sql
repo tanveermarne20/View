0ffb7868722af01efe17fd2a66ac007c3cb01631
tanveermarne20 <>
2024-03-04 16:51:56 +0530

    records added

---
+++ TableCreation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE AutomobileParts (
+    PartID INT PRIMARY KEY,
+    PartName VARCHAR(100),
+    PartType VARCHAR(50),
+    Manufacturer VARCHAR(50),
+    Price DECIMAL(10, 2),
+    InStock INT
+);
+
